eeeecd7b36103d71cc941a32c912a2a7f1c822e0
efreestone <efreestone@fullsail.edu>
2014-07-31 14:15:33 -0600

    Rebuilt event input form based on old MiU app with various tweaks and some elements removed. jQuery and JQM aren't working quite right at the moment but will have to come back to those

--- MDF3Project4/assets/js/main.js
+++ MDF3Project4/assets/js/main.js
@@ -6,5 +6,119 @@ Project 4
 */
 
 $('#index').on('pageinit', function(){
-	//code needed for index page goes here
+	var myForm = $("#eventForm"),
+		popErrors = $('#popErrors');
+			
+		//Validate the event form
+	    myForm.validate({
+		invalidHandler: function(form, validator) {
+			//Pop up error dialog box
+			popErrors.click(); 
+			var html = '';
+			for(var key in validator.submitted) {
+				//Search for required elements not filled out
+				var label = $('label[for^="'+ key +'"]') 
+				var legend = label.closest('fieldset').find('ui-controlgroup-label');
+				var fieldName = legend.length ? legend.text() : label.text();
+				html += '<li>'+ fieldName +'</li>';
+			};
+			//Creat pop up error text
+			$("#formErrors ul").html(html);
+		},
+		submitHandler: function() {
+			var data = myForm.serializeArray();
+		}
+	});
+	
+	//getElementById Function
+    function gE(x) {
+        var theElement = document.getElementById(x);
+        return theElement;
+    };
+    
+    //Find value of selected radio button.
+    function getSelectedRadio() {
+        var radios = document.forms[0].attend;
+        for(var i=0; i<radios.length; i++) {
+            if(radios[i].checked) {
+                attendValue = radios[i].value;
+            }
+        }
+    };
+    
+    //To hide or show input fields and controls
+    function toggleControls(n) {
+        switch(n) {
+            case "on":
+                gE("eventForm").style.display = "none";
+                gE("clearData").style.display = "inline";
+                gE("displayData").style.display = "none";
+                gE("addNew").style.display = "inline";
+                break;
+            case "off":
+                gE("eventForm").style.display = "block";
+                gE("clearData").style.display = "inline";
+                gE("displayData").style.display = "inline";
+                gE("addNew").style.display = "none";
+                gE("items").style.display = "none";
+                break;
+            default:
+                return false;
+        }    
+    };
+    
+    //Validate entry fields
+    function validate(e) {
+    	//Define elements we want to check
+    	var getEvents = gE("events");
+    	var getEvdate = gE("evdate");
+    	var getEvinfo = gE("evinfo");
+    	
+    	//Reset error messages
+    	errMsg.innerHTML = "";
+    	
+    	//Get error messages
+    	var messageAry = [];
+    	//Event validation
+    	if(getEvents.value === "--Choose An Event Type--") {
+	    	var eventsError = "Please choose an event type.";
+	    	getEvents.style.border = "1px solid red";
+	    	messageAry.push(eventsError);
+    	}
+	    
+	    //Event date validation
+	    if(getEvdate.value === "") {
+		    var evDateError = "Please choose a date for the event.";
+	    	getEvdate.style.border = "1px solid red";
+	    	messageAry.push(evDateError);
+	    }
+	    
+	    //Event info validation
+	    if(getEvinfo.value === "") {
+		    var evInfoError = "Please add a brief description of the event";
+	    	getEvinfo.style.border = "1px solid red";
+	    	messageAry.push(evInfoError);
+	    }
+	    	
+	    //If there were errors, display the on the screen
+	    if(messageAry.length >= 1) {
+		    for(var i=0, j=messageAry.length; i < j; i++) {
+			    var txt = document.createElement("li");
+			    txt.innerHTML = messageAry[i];
+			    errMsg.appendChild(txt);
+		    }
+		    e.preventDefault();
+		    return false;
+	    }else{
+		    //If all is ok, save our data! Send the key value that came form the editData function.
+		    //Remember this key value was passed through the editSubmit event listener as a property
+		    //saveData(this.key);
+	    }	   
+    };
+    
+    //Variable defaults
+    var eventTypes = ["--Choose An Event Type--", "Anniversary", "Appointment", "Birthday", "Meeting", "Other"],
+        attendValue,
+        errMsg = gE("errors");
+	
 });	
--- MDF3Project4/src/com/elijahfreestone/mdf3project4/MainActivity.java
+++ MDF3Project4/src/com/elijahfreestone/mdf3project4/MainActivity.java
@@ -10,30 +10,36 @@
 
 package com.elijahfreestone.mdf3project4;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+// TODO: Auto-generated Javadoc
+@SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends Activity {
 	WebView myWebView;
 
-	/* (non-Javadoc)
+	/* (non-Javadoc) 
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
-	@Override
+	@Override  
 	protected void onCreate(Bundle savedInstanceState) {
+		String backgroundColor = "#7ED0E0";
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
 		//Grab webview and load test url
 		myWebView = (WebView) findViewById(R.id.myWebView);
 		myWebView.loadUrl("file:///android_asset/index.html");
+		myWebView.setBackgroundColor(Color.parseColor(backgroundColor));
 		
 		//Grab web settings and enable javascript
 		WebSettings webSettings = myWebView.getSettings();
-		webSettings.setJavaScriptEnabled(true);  
- 
-	}       
+		webSettings.setJavaScriptEnabled(true);             
+  
+	}                            
 
 }
